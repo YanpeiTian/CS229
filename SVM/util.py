10337a48adcc80e683ca3f7f535a2abed1176cfb
Scott_Mac <scott_mac@DN0a22b6a2.SUNet>
2019-12-04 15:23:29 -0800

    svm

---
+++ SVM/util.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def load_dataset(csv_path, label_col='Label'):
+    """Load dataset from a CSV file.
+
+    Args:
+         csv_path: Path to CSV file containing dataset.
+         label_col: Name of column to use as labels (should be 'y' or 't').
+
+    Returns:
+        xs: Numpy array of x-values (inputs).
+        ys: Numpy array of y-values (labels).
+    """
+
+    # Load headers
+    with open(csv_path, 'r') as csv_fh:
+        headers = csv_fh.readline().strip().split(',')
+
+    # Load features and labels
+    x_cols = [i for i in range(1,len(headers)) if headers[i]!='Label']
+    l_cols = [i for i in range(1,len(headers)) if headers[i] == 'Label']
+    inputs = np.loadtxt(csv_path, delimiter=',', skiprows=1, usecols=x_cols)
+    labels = np.loadtxt(csv_path, delimiter=',', skiprows=1, usecols=l_cols)
+
+    if inputs.ndim == 1:
+        inputs = np.expand_dims(inputs, -1)
+
+    return inputs, labels
